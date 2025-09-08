8ddfe8c7a850614fe427700646c8ff95470ee6ed
최이서 <chtoqur@gmail.com>
2025-09-08 17:44:21 +0900

    feat: 템플릿 레포지토리 생성 (#5)

--- src/main/java/app/snapshot/qure/template/repository/ITemplateRepository.java
+++ src/main/java/app/snapshot/qure/template/repository/ITemplateRepository.java
@@ -1,15 +1,14 @@
 package app.snapshot.qure.template.repository;
 
-import app.snapshot.qure.template.dto.DomainType;
-import app.snapshot.qure.template.dto.TemplateDto;
-import org.apache.ibatis.annotations.Param;
+import app.snapshot.qure.template.model.Template;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 public interface ITemplateRepository {
-    List<TemplateDto> getTemplateList();
-    List<TemplateDto> searchTemplates(@Param("domain") DomainType domain, @Param("keyword") String keyword);
-    int insertTemplate(TemplateDto templateDto);
-    int updateTemplate(TemplateDto templateDto);
-    int deleteTemplate(Long id);
+    List<Template> getTemplateList();
+//    List<TemplateDto> searchTemplates(@Param("domain") DomainType domain, @Param("keyword") String keyword);
+//    int insertTemplate(TemplateDto templateDto);
+//    int updateTemplate(TemplateDto templateDto);
+//    int deleteTemplate(Long id);
 }
