b7822248f0361ac47c368518c8b330d8339220a1
HeHelee <selly3109@naver.com>
2025-09-11 13:07:32 +0900

    fix: 시큐리티 설정 오류 해결

--- src/main/java/app/snapshot/qure/config/SecurityConfig.java
+++ src/main/java/app/snapshot/qure/config/SecurityConfig.java
@@ -39,11 +39,14 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
+                .csrf(csrf -> csrf.disable())
+
                 .oauth2Login(oauth -> oauth
                         .loginPage("/")   // 카카오 진입 전에 JSP 경로 잡아주기
                         .defaultSuccessUrl("/manager/home")
                         .userInfoEndpoint(userInfo -> userInfo.userService(kakaoOAuth2UserService))
                 );
+
 
         return http.build();
     }
